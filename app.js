d8d1a34989a644bfd3df97d782a11a2ae148e870
Bilal-Nadeem-Ahmed <61297097+Megamind44@users.noreply.github.com>
2020-10-30 14:57:49 +0000

    finished

---
+++ app.js
@@ -0,0 +1,8 @@
+const submitBtn= document.getElementById("submit");
+
+submitBtn.addEventListener("click", function(){
+  const message= document.getElementById("thanks"
+  );
+  message.innerText="Thanks For Your Response!";
+  console.log("hi")
+})
